39fde899902ef42d1848f7489f114eac94ce0af6
hyery <hyelee-@naver.com>
2020-10-19 10:27:46 +0900

    ..

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 function onEnter() {
 	$(this).find(".sub-wrap").show();
 }
@@ -5,36 +6,35 @@ function onLeave() {
 	$(this).find(".sub-wrap").hide();
 }
 
-/** Main Navi 생성 **********************/
+/* Main Navi 생성 */
 $.get('../json/navi.json', onNaviLoad);
-function onNaviLoad(r) {
-	console.log(r.navs);
-	var html = '';
-	for(var i in r.navs) {
-		html  = '<div class="navi '+r.navs[i].class+'">';
-		html += '<span class="title">'+r.navs[i].title+' <i class="fa fa-angle-down"></i></span>';
-		html += '<div class="sub-wrap">';
-		for(var j in r.navs[i].subs) {
-			html += '<div class="sub">';
-			html += '<div class="title">1. HOME DEFAULT</div>';
-			html += '<div class="cont-img"><img src="../img/default.jpg" alt="그림" class="w-100"></div>';
-			html += '</div>';
-		}
-		html += '</div>';
-		html += '</div>';
+funciton onNaviLoad(r){
+	console.log(r.navw);
+	for(var i in r.navs){
+		var html='<div class="navi">'; any
+		html += '<span class="title">'+r.navs.title+' <i class="fa fa-angle-down"></i></span>';
+		html +='<div class="sub-wrap">';
+		for()
+		html +=
+		html +=
+		html +=
+		html +=
+		html +=
+		html +=
+		html +=
+		html +=
+		html +=
+	}
 		$(".navi-wrap").append(html);
 	}
-	$(".navi-wrap > .navi").mouseenter(onEnter);
-	$(".navi-wrap > .navi").mouseleave(onLeave);
+		$(".navi-wrap > .navi").mouseenter(onEnter);
+		$(".navi-wrap > .navi").mouseleave(onLeave);
+	}
 }
-/*
-<div class="navi">
-	<span class="title">HOME <i class="fa fa-angle-down"></i></span>
-	<div class="sub-wrap">
-		<div class="sub">
-			<div class="title">1. HOME DEFAULT</div>
-			<div class="cont-img"><img src="../img/default.jpg" alt="그림" class="w-100"></div>
-		</div>
-	</div>
-</div>
-*/
+/* 
+div class="navi">
+<spnan class="title">HOME<i class="fa fa-angle-down"></i></span>
+<div class="sub-wrap">
+div class="sub">
+
+ */
