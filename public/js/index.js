4ff4476c1bfa8e0197f5415b1884455cc59cd9a1
hyery <hyelee-@naver.com>
2020-10-19 10:38:00 +0900

    ..

--- public/js/index.js
+++ public/js/index.js
@@ -8,29 +8,28 @@ function onLeave() {
 
 /* Main Navi 생성 */
 $.get('../json/navi.json', onNaviLoad);
-funciton onNaviLoad(r){
+function onNaviLoad(r){
 	console.log(r.navw);
 	for(var i in r.navs){
 		var html='<div class="navi">'; any
 		html += '<span class="title">'+r.navs.title+' <i class="fa fa-angle-down"></i></span>';
 		html +='<div class="sub-wrap">';
-		for()
-		html +=
-		html +=
-		html +=
-		html +=
-		html +=
-		html +=
-		html +=
-		html +=
-		html +=
+		for(var j in r.navs[i].subs){
+		html +='<div class="sub">';
+		html +='<div class="title">1. HOME DEFAULT</div>';
+		html +='<div class="cont-img"><img src="../img/default.jpg" alt="그림" class="w-100"></div>';
+		html +='</div>';
+	}
+		html +='<div>';
+		html +='<div>';
+		
 	}
 		$(".navi-wrap").append(html);
 	}
 		$(".navi-wrap > .navi").mouseenter(onEnter);
 		$(".navi-wrap > .navi").mouseleave(onLeave);
-	}
-}
+	
+
 /* 
 div class="navi">
 <spnan class="title">HOME<i class="fa fa-angle-down"></i></span>
