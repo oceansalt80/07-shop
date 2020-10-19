ce019e5209f00ef10d18f6a41dc7ff2427b96e68
hyery <hyelee-@naver.com>
2020-10-19 11:39:29 +0900

    ..

--- public/js/index.js
+++ public/js/index.js
@@ -12,18 +12,21 @@ function onNaviLoad(r){
 	console.log(r.navw);
 	for(var i in r.navs){
 		var html='<div class="navi">'; any
-		html += '<span class="title">'+r.navs.title+' <i class="fa fa-angle-down"></i></span>';
+		html += '<div class="title">'+r.navs.title+' <i class="fa fa-angle-down"></i>';
+		if(r.navi[i].icon !=''){
+			html +='<div class="icon">+r.navs[i].icon;
+			html +='<i class="caret-right"></i>';
+			html +='</div>';
+			</span>';
 		html +='<div class="sub-wrap">';
 		for(var j in r.navs[i].subs){
 		html +='<div class="sub">';
-		html +='<div class="title">1. HOME DEFAULT</div>';
+		html +='<div class="title">+r.navs[i].subs[j].title+'</div>';
 		html +='<div class="cont-img"><img src="../img/default.jpg" alt="그림" class="w-100"></div>';
 		html +='</div>';
 	}
-		html +='<div>';
-		html +='<div>';
-		
-	}
+		html +='<div>';//.sub-wrap
+		html +='<div>';//.navi
 		$(".navi-wrap").append(html);
 	}
 		$(".navi-wrap > .navi").mouseenter(onEnter);
